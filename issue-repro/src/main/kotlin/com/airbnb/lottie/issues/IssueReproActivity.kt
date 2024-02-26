7981f35ed9ca7d6d0fd71b478cd625ddec680a5d
john6 <john6.lq@gmail.com>
2024-02-26 17:37:22 +0800

    [WN] add code to demonstrate replay issue

--- issue-repro/src/main/kotlin/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/kotlin/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -1,7 +1,11 @@
 package com.airbnb.lottie.issues
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.airbnb.lottie.issues.databinding.IssueReproActivityBinding
 
 class IssueReproActivity : AppCompatActivity() {
@@ -10,5 +14,14 @@ class IssueReproActivity : AppCompatActivity() {
         val binding = IssueReproActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // Reproduce any issues here.
+        binding.animationView.setAnimation(R.raw.heart)
+        binding.animationView.setOnClickListener {
+            if((it as LottieAnimationView).drawable is LottieDrawable){
+                it.setImageDrawable(ContextCompat.getDrawable(this, R.mipmap.ic_launcher))
+            }else{
+                it.setAnimation(R.raw.heart)
+                it.playAnimation()
+            }
+        }
     }
 }
